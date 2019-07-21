54f63feec5236740bb0fb36ef44f5228f1ed592b
parishrutk <51824177+parishrutk@users.noreply.github.com>
2019-07-21 21:22:47 +0530

    Moved code to CodingChallange folder.

---
+++ CodingChallange/src/main/java/com/paypaycorp/queue/Queue.java
@@ -0,0 +1,17 @@
+package com.paypaycorp.queue;
+
+public interface Queue<T> {
+
+    Queue<T> enQueue(T t);
+
+    //#Removes the element at the beginning of the immutable queue, and returns the new queue.
+    Queue<T> deQueue();
+
+    T head();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int getSize();
+}
